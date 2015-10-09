281566a19501669958eb82f9a32fc648c904a922
Francisco Zuniga <baycfran42@gmail.com>
2015-10-09 13:51:52 -0600

    Version mas reciente y corriendo, login y navegar integrados

--- soft/eccimovies/app/Test/Case/Model/UserTest.php
+++ soft/eccimovies/app/Test/Case/Model/UserTest.php
@@ -2,12 +2,8 @@
 class UserTest extends CakeTestCase {
     public $fixtures = array('app.user');
 	
-<<<<<<< HEAD
 	public function testMyFunction()
 	{
-=======
-	public function testMyFunction() {
->>>>>>> bc95873fc318bae3dd4d63cbd91d7440ee2b8cc7
         $this->loadFixtures('User');
     }
 }
